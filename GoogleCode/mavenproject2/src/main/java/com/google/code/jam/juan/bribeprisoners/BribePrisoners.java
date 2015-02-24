e5cecb776b14df1f9943e986405ce15606aed366
jdavidagudelo <jdaaa2009@gmail.com>
2015-02-23 21:11:10 -0500

    Dynamic programming and recursion, must be practiced at all cost, quickly, DAMN

--- GoogleCode/mavenproject2/src/main/java/com/google/code/jam/juan/bribeprisoners/BribePrisoners.java
+++ GoogleCode/mavenproject2/src/main/java/com/google/code/jam/juan/bribeprisoners/BribePrisoners.java
@@ -23,125 +23,74 @@ import java.util.*;
 public class BribePrisoners {
 
     private Integer P;
-    private Integer Q;
-    private class Node implements Comparable<Node>
-    {
-        public int cost;
-        public boolean[] cells;
-        public int q;
-        public int prisoner;
+    private Set<Integer> Q;
+    private int[] q;
+    private int memo[][];
 
-        public Node(int cost, boolean[] cells, int q, int prisoner) {
-            this.cost = cost;
-            this.cells = cells;
-            this.q = q;
-            this.prisoner = prisoner;
-        }
-        
-        public int compareTo(Node o)
-        {
-            if(o.cost != cost)
-            {
-                return cost - o.cost;
-            }
-            if(q != o.q)
-            {
-                return q - o.q;
-            }
-            if(prisoner != o.prisoner)
-            {
-                return prisoner - o.prisoner;
-            }
+    public int getCost(int n, int m) {
+        if (m - n < 2) {
+            memo[n][m] = 0;
             return 0;
         }
-        
+        if (memo[n][m] != -1) {
+            return memo[n][m];
+        }
+        int min = Integer.MAX_VALUE;
+        for (int i = n + 1; i < m; i++) {
+            int calc = (q[i] - q[n] - 1) + (q[m] - q[i] - 1);
+            calc = calc + getCost(n, i) + getCost(i, m);
+            min = Math.min(min, calc);
+        }
+        memo[n][m] = min;
+        return min;
     }
-    private boolean over(boolean cells[])
-    {
-        int count = 0;
-        for(boolean b : cells)
-        {
-            if(b)
-            {
-                count++;
-            }
-        }
-        return count == Q;
-    }
-    private int getCost(boolean[] cells, int p)
-    {
-        int cost = 0;
-        for(int i = p+1; i < cells.length && cells[i]; i++)
-        {
-            cost++;
-        }
-        for(int i = p-1; i >= 0 && cells[i]; i++)
-        {
-            cost++;
-        }
-        return cost;
-    }
-    public int bfs()
-    {
-        TreeSet<Node> queue = new TreeSet<>();
-        for(int p = 0; p < P; p++)
-        {
-            
-            boolean cells[] = new boolean[P];
-            Arrays.fill(cells, true);
-            cells[p] = false;
-            int cost = getCost(cells, p);
-            queue.add(new Node(cost, cells, 1, p));
-        }
-        while(!queue.isEmpty())
-        {
-            Node current = queue.first();
-            queue.remove(current);
-            if(current.q == Q)
-            {
-                return current.cost;
-            }
-            for(int i = 0; i < current.cells.length; i++)
-            {
-                if(current.cells[i])
-                {
-                    int cost = getCost(current.cells, i);
-                    boolean[] cells = Arrays.copyOf(current.cells, current.cells.length);
-                    cells[i] = false;
-                    queue.add(new Node(current.cost+cost, cells, current.q+1, i));
-                }
-            }
-        }
-        return 0;
-    }
+
     public static void process(String inputPath, String outputPath) throws FileNotFoundException, IOException {
         List<BribePrisoners> list = new ArrayList<BribePrisoners>();
-        
         try (BufferedReader reader = new BufferedReader(new FileReader(new File(inputPath)))) {
             try (BufferedWriter writer = new BufferedWriter(new FileWriter(new File(outputPath)))) {
                 String line = reader.readLine();
-                while (line != null) {
+                int count = Integer.parseInt(line);
+                int i = 0;
+                while (line != null && i < count) {
                     //Here you read and write everything, the reader and writer are automatically closed by java 7
-                    int count = Integer.parseInt(line);
+
                     line = reader.readLine();
-                    for(int i = 0; i < count && line != null; i++)
-                    {
+                    for (i = 0; i < count && line != null; i++) {
                         BribePrisoners bp = new BribePrisoners();
                         String split[] = line.split(" ");
                         bp.P = Integer.parseInt(split[0]);
-                        bp.Q = Integer.parseInt(split[1]);
+                        bp.Q = new HashSet<Integer>();
+
                         list.add(bp);
+                        line = reader.readLine();
+                        split = line.split(" ");
+                        for (String s : split) {
+                            bp.Q.add(Integer.parseInt(s));
+                        }
+                        bp.Q.add(0);
+                        bp.Q.add(bp.P + 1);
+                        bp.q = new int[bp.Q.size()];
+                        int k = 0;
+                        for (int x : bp.Q) {
+                            bp.q[k] = x;
+                            k++;
+                        }
+                        Arrays.sort(bp.q);
+                        bp.memo = new int[bp.q.length][bp.q.length];
+                        for (int a[] : bp.memo) {
+                            Arrays.fill(a, -1);
+                        }
                         line = reader.readLine();
                     }
                 }
-                int i = 0;
-                for(BribePrisoners bp : list)
-                {
-                     StringBuilder sb = new StringBuilder();
-                        sb.append("Case #").append(i + 1).append(": ").append(bp.bfs());
-                        writer.append(sb.toString());
-                        writer.newLine();
-                        i++;
+                i = 0;
+                for (BribePrisoners bp : list) {
+                    StringBuilder sb = new StringBuilder();
+                    sb.append("Case #").append(i + 1).append(": ").append(bp.getCost(0, bp.q.length - 1));
+                    writer.append(sb.toString());
+                    writer.newLine();
+                    i++;
                 }
             }
         }
@@ -152,7 +101,7 @@ public class BribePrisoners {
         //change it with your specific resources for each input.
         String fileLarge = classLoader.getResource("com/google/code/jam/juan/bribeprisoners/large.in").getFile();
         String fileSmall = classLoader.getResource("com/google/code/jam/juan/bribeprisoners/small.in").getFile();
-        //process(fileLarge, "large.txt");
+        process(fileLarge, "large.txt");
         process(fileSmall, "small.txt");
     }
 }
